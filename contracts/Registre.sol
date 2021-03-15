13007e93f2fd49ab644d9bab851ea6932dc63867
Renaud <renaud.forestier@gmail.com>
2021-03-15 15:31:16 +0100

    update

--- contracts/Registre.sol
+++ contracts/Registre.sol
@@ -2,37 +2,37 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract RegistreContrat {
-    
+
    struct DetailContrat {
       address proprietaire;
-      address contratAddresse;
+      address contratAdresse;
       uint16 version;
    }
 
    mapping(string => DetailContrat) registre;
-   
+
    function enregistrerContrat(string memory nom, address adresse, uint16 version) public  {
-       
+
       require(version >= 1);
-      
+
       DetailContrat memory info = registre[nom];
 
-       if (info.contratAddresse == address(0)) {
+       if (info.contratAdresse == address(0)) {
           info = DetailContrat({
              proprietaire: msg.sender,
-             contratAddresse: adresse,
+             contratAdresse: adresse,
              version: version
           });
        } else {
-          require(info.proprietaire == msg.sender); 
-          info.contratAddresse = adresse;
+          require(info.proprietaire == msg.sender);
+          info.contratAdresse = adresse;
           info.version = version;
        }
        registre[nom] = info;
 
    }
-   
+
     function contratDetails(string memory nom)  public view returns(address, uint16) {
-      return (registre[nom].contratAddresse, registre[nom].version);
+      return (registre[nom].contratAdresse, registre[nom].version);
     }
 }
